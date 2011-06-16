b9d93f4f866c950c7228ba852adffe4b48b5baa3
Vid <vid@uoregon.edu>
2011-06-16 16:59:20 -0700

    uncomment autoload in init

--- projectmenuobjectcount/activecollab/application/modules/projectmenuobjectcount/init.php
+++ projectmenuobjectcount/activecollab/application/modules/projectmenuobjectcount/init.php
@@ -6,10 +6,8 @@
    */
   define('PROJECTMENUOBJECTCOUNT_MODULE', 'projectmenuobjectcount');
   define('PROJECTMENUOBJECTCOUNT_MODULE_PATH', APPLICATION_PATH . '/modules/projectmenuobjectcount');
-	/*
+	
   set_for_autoload(array(
 		'Projectmenuobjectcount' => PROJECTMENUOBJECTCOUNT_MODULE_PATH . '/models/Projectmenuobjectcount.class.php',
 	));
-
-	*/
 ?>
