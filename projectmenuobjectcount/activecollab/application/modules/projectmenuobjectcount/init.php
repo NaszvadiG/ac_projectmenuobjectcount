1e9a07fd01f825973f2758109761fa54219d8388
Vid <vid@uoregon.edu>
2011-06-16 16:57:46 -0700

    uncomment define path

--- projectmenuobjectcount/activecollab/application/modules/projectmenuobjectcount/init.php
+++ projectmenuobjectcount/activecollab/application/modules/projectmenuobjectcount/init.php
@@ -4,11 +4,10 @@
    *
    * @package activeCollab.modules.projectmenuobjectcount
    */
-  /*
   define('PROJECTMENUOBJECTCOUNT_MODULE', 'projectmenuobjectcount');
   define('PROJECTMENUOBJECTCOUNT_MODULE_PATH', APPLICATION_PATH . '/modules/projectmenuobjectcount');
-	
-	set_for_autoload(array(
+	/*
+  set_for_autoload(array(
 		'Projectmenuobjectcount' => PROJECTMENUOBJECTCOUNT_MODULE_PATH . '/models/Projectmenuobjectcount.class.php',
 	));
 
