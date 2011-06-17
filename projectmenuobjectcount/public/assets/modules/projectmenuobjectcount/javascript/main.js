c58b4a163af4536aa9cc638d013181f7a70b31c3
Vid <vid@uoregon.edu>
2011-06-16 17:23:07 -0700

    test3 of main.js adding some js to identify tab and append to span. not:() selector needs work

--- projectmenuobjectcount/public/assets/modules/projectmenuobjectcount/javascript/main.js
+++ projectmenuobjectcount/public/assets/modules/projectmenuobjectcount/javascript/main.js
@@ -1,4 +1,11 @@
 //alert('projectmenuobjectcount.js loaded'); //tested. Works
 $(document).ready(function(){  /*run after jQuery loads*/
 	//alert('jquery Ready'); //tested. Works
+	/*
+	javascript: 
+	if($('#logged_user span.inner').text().search('Welcome back Vid') !== -1 ) {
+		$('#tabs div.container ul li a span:not(count)').addClass('count').append(' (#)'); //this doesn't work quite right. need to work on the not:()
+	}
+	void(0);
+	*/
 });  /*end ready*/
