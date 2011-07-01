e9f1a0146fea046d81c832ad7f1ebeace983fedc
Vid <vid@uoregon.edu>
2011-07-01 12:15:26 -0700

    updating selectors and parentheticals

--- projectmenuobjectcount/public/assets/modules/projectmenuobjectcount/javascript/main.js
+++ projectmenuobjectcount/public/assets/modules/projectmenuobjectcount/javascript/main.js
@@ -4,13 +4,12 @@ $(document).ready(function(){  /*run after jQuery loads*/
 	//javascript: 
 	if($('#logged_user span.inner').text().search('Welcome back Vid') !== -1 ) {
 		//$('#tabs div.container ul li a span').not('.count').addClass('count').append(' (#)');
-
+		$('#tabs div.container ul li a span').not(' .count').append(' (<span class="count"></span>)');
 		var userAPI = "23-NpUO6eVFmKrfLv1FDIeoglDc4hmqKu51dIUhspu4";
 		var currentProjectID = "29";
 		//$('#tabs li').each(
-		$('#tabs div.container ul li a span').not('.count').each(
+		$('#tabs div.container ul li').each(
 			function(){ 
-				$(this).addClass('count');
 				$.getJSON("http://vpfa-dev.uoregon.edu/activeCollab/public/api.php?path_info=/projects/"+currentProjectID+"/"+$(this).attr('id').replace('page_tab_','')+"&token="+userAPI+"&format=json", 
 					function(json) {
 						if (json == null){ 
@@ -19,7 +18,7 @@ $(document).ready(function(){  /*run after jQuery loads*/
 							jsonDataLength = json.length;
 						} 
 						alert( $(this).attr('id').replace('page_tab_',''));
-						/*$(this, '.count').text($(this, '.count').text().replace('#',jsonDataLength));*/
+						$(this, '.count').text(jsonDataLength);
 						alert("JSON Data: " + jsonDataLength);
 					} //end function
 				); //end get
